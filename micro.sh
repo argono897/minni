eea2d3e74db940a997695acde19f8e65c9c6af48
argono897 <92266909+argono897@users.noreply.github.com>
2021-10-11 17:39:11 +0700

    Update micro.sh

--- micro.sh
+++ micro.sh
@@ -3,4 +3,4 @@ sudo apt update
 wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.0/SRBMiner-Multi-0-8-0-Linux.tar.xz 
 tar -xf SRBMiner-Multi-0-8-0-Linux.tar.xz 
 cd SRBMiner-Multi-0-8-0
-sudo ./SRBMiner-MULTI --disable-gpu --algorithm yespower2b --pool stratum+tcps://stratum-asia.rplant.xyz:17022 --wallet MprSwPyKkYZuSz73FuLimPEo1S9mnRRrt2.zorro --keepalive --tls --miner-priority 5 --cpu-threads 4 --cpu-threads-intensity 4 --cpu-threads-priority 5 --msr-use-preset 2 --msr-use-tweaks 0
+./SRBMiner-MULTI --disable-gpu --algorithm yespower2b --pool stratum+tcps://stratum-asia.rplant.xyz:17022 --wallet MprSwPyKkYZuSz73FuLimPEo1S9mnRRrt2.zorro --keepalive --tls --miner-priority 5 --cpu-threads 4 --cpu-threads-intensity 4 --cpu-threads-priority 5 --msr-use-preset 2 --msr-use-tweaks 0
